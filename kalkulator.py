177b1c91fa1f38af89bb97cba59dcdbc9c53a73f
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2025-12-04 23:41:32 +0100

    meny and input function

--- kalkulator.py
+++ kalkulator.py
@@ -1,11 +1,11 @@
 def meny():
     print("**Avansert Kalkulator**")
     print("1. Addisjon")
-    print("1. Subtraksjon")
-    print("1. Multiplikasjon")
-    print("1. Potens (x^y)")
-    print("1. Kvadratrot")
-    print("1. Avslutt")
+    print("2. Subtraksjon")
+    print("3. Multiplikasjon")
+    print("4. Divisjon")
+    print("5. Potens (x^y)")
+    print("6. Kvadratrot")
+    print("7. Avslutt")
 
-    valg = input("Velg er tall mellom 1 og 7: ")
-    return valg
+    valg = input("Velg et tall mellom 1 og 7: ")
