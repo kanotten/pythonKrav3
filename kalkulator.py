f2f517e85f32dae8b50f60b524ee54330faa1ba6
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2025-12-04 23:12:31 +0100

    python calc menu setup

---
+++ kalkulator.py
@@ -0,0 +1,11 @@
+def meny():
+    print("**Avansert Kalkulator**")
+    print("1. Addisjon")
+    print("1. Subtraksjon")
+    print("1. Multiplikasjon")
+    print("1. Potens (x^y)")
+    print("1. Kvadratrot")
+    print("1. Avslutt")
+
+    valg = input("Velg er tall mellom 1 og 7: ")
+    return valg
